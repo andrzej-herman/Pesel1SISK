63b74645c53bd8f98808c2584b68ca09327fed2c
Andrzej Herman <mail@aherman.org>
2022-05-15 11:14:21 +0200

    15.05.2022

---
+++ Domain/CitizenData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public  class CitizenData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int Sex { get; set; }
+    }
+}
